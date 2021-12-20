2fc40c5a92c6d1a0a90b3d3a713e429b8028dac4
aldofebriii <aldothepro@gmail.com>
2021-12-20 08:34:53 +0700

    update guys on cli

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const server = require('./lib/server');
 const worker = require('./lib/worker');
+const cli = require('./lib/cli');
 
 const app = {};
 
 app.init = function(){
     //Initialize Server
     server.init();
-
+    //Initialize Worker
     worker.init();
+    //Start the cli at the last one so we set into the asynchronus
+    setTimeout(function(){
+        cli.init();
+    },0)
 };
 
 app.init();
